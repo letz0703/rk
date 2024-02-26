3196caf96b719ae8f7e27dabeb80914a98a3049d
ManI <letz0703@gmail.com>
2024-02-26 22:27:20 +0900

    menu design

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -21,12 +21,22 @@ const Links = () => {
       path: "/blog"
     }
   ]
+  const session = true
+  const isAdmin = true
 
   return (
     <div className={styles.links}>
       {links.map(link => (
         <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
+          <button className={` ${styles.logout}`}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{title: "Login", path: "/login"}} />
+      )}
     </div>
   )
 }
