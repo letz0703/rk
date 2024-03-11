4fcf40e98ae1b8978473916609c9f4e56c32f7ed
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-03-11 17:27:17 +0900

    2024.03.11/월

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,13 +2,35 @@ import Image from "next/image"
 import styles from "./about.module.css"
 const AboutPage = () => {
   return (
-    <div>
-      <div className={styles.imgContainer} />
-      <Image
-        src="https://i.pinimg.com/736x/8d/8f/c7/8d8fc761bbac70f07973e9bd7d25eca7.jpg"
-        alt="about"
-        fill
-      />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>Love & Free</h1>
+        <p className={styles.description}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id nam
+          sapiente quaerat eos molestias repellat commodi blanditiis est quas
+          ratione?
+        </p>
+      </div>
+      <div className={styles.boxes}>
+        <div className={styles.boxes}>
+          <h1>10 K+</h1>
+          <p> Years of experience </p>
+        </div>
+        <div className={styles.boxes}>
+          <h1>10 K+</h1>
+          <p> Years of experience </p>
+        </div>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p> Years of experience </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="about" fill />
+      </div>
     </div>
   )
 }
