250ba425ec8bb2c9db3663fcc22e81316bba633d
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-03-13 12:30:47 +0900

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,27 @@
-const Page = () => {
+import Image from "next/image"
+import styles from "./contact.module.css"
+const ContactPage = () => {
   return (
-    <div>
-      <h2>Page</h2>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" fill alt="" className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name and Sirname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
-export default Page
+export default ContactPage
