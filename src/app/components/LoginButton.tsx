dcdffa256644f0f53d5b214c3244b4ec66ebe7eb
ManI <letz0703@gmail.com>
2025-06-12 19:05:08 +0900

    2025.06.12 목 pm

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,11 +1,44 @@
 "use client"
 
-import {login} from "../../api/firebase"
+import {useState, useEffect} from "react"
+import {login, logout} from "../../api/firebase"
+import {User} from "firebase/auth"
 
 export default function LoginButton() {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user")
+    if (storedUser) {
+      setUser(JSON.parse(storedUser))
+    }
+  }, [])
+
+  const handleLogin = async () => {
+    const userData = await login()
+    if (userData) {
+      setUser(userData)
+      localStorage.setItem("user", JSON.stringify(userData))
+    }
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem("user")
+    logout()
+    setUser(null)
+  }
+
   return (
-    <button className="text-blue-700" onClick={login}>
-      login
-    </button>
+    <div>
+      {user ? (
+        <button className="text-red-500" onClick={handleLogout}>
+          logout
+        </button>
+      ) : (
+        <button className="text-blue-700" onClick={handleLogin}>
+          login
+        </button>
+      )}
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <AlertClient />
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <nav>
+            <LoginButton />
+          </nav>
           <Image
             className="dark:invert"
             src="/logo rk.svg"
@@ -22,35 +25,46 @@ export default function Home() {
             width={180}
             height={38}
             priority
-          />{" "}
-          <h1 className="text-2xl text-blue-500 text-bold">
-            Have Your Own Song !!
+          />
+          <div>
+            I Invite You to ...
+            <a
+              href="https://suno.com/invite/@rainskiss_o"
+              className="btn bg-blue-200 p-1 pt-1"
+            >
+              RainsKiss @SUNO
+            </a>
+            <br />
+            <code className="mt-2 text-green-200">
+              When you sign up and create 10 songs, We both receive 250 free
+              credits.
+            </code>
+          </div>
+          <h1 className="text-2xl text-orange-500 text-bold">
+            Make Your Own Song <br />
+            <span className="text-sm text-white">
+              & Let Me Mix or Master Them
+            </span>
           </h1>
-          <nav>
-            <LoginButton />
-          </nav>
           <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
             <li className="mb-2">
-              I normalize audio and upscale clips as a hobby.
+              I equalize songs. Buy me a coffee if you like it.
             </li>
+            <li className="mb-2">I upscale clips I like.</li>
             <li>
-              I create copyright-free music and wallpapers (for non-commercial
-              use only.)
+              I create copyright-free music and wallpapers for subscribers (for
+              non-commercial use only.)
             </li>
-            <li>
-              I make songs and short videos. Send me any lyrics and a reference
-              song, and I will create one free for you.
-              <br />
-              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                Just subscribe and email me your favorite link, reference song,
-                and lyrics, and let me know.
-                <br />
-                I'll check your favorites and make a short video for you during
-                my free time. The short I create will be posted on my YouTube
-                channel @rainskiss.m.
-              </code>
-            </li>
+            <li>I will make a 3D doll of you. Email me and let me know.</li>
           </ol>
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            Just subscribe and email me your favorite link, reference song, and
+            lyrics, and let me know.
+            <br />
+            I'll review your favorite links and create a short video for you in
+            my spare time. The short I create will be posted on my YouTube
+            channel @rainskiss.m if you don't mind 🦋 Let me know when it's sent
+          </code>
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -70,7 +84,8 @@ export default function Home() {
             </a>
           </div>
           <div>
-            If you satisfied with the result or want anther, Buy Me a Coffee 😃
+            If you are satisfied with the result or want another, buy me a
+            coffee 😃
           </div>
         </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
