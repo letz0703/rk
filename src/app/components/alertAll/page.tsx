f295d8f6c3161a521f4c7e98550ac5baa3e25a34
ManI <letz0703@gmail.com>
2025-06-23 23:04:10 +0900

    ..

--- src/app/components/alertAll/page.tsx
+++ src/app/components/alertAll/page.tsx
@@ -1,16 +1,17 @@
+"use client"
 import dynamic from "next/dynamic"
 
 // AlertAll 컴포넌트 동적 로드
-const AlertAll=dynamic(() => import("../AlertAll"), {
-	ssr: false, // 클라이언트에서만 렌더링
+const AlertAll = dynamic(() => import("../AlertAll"), {
+  ssr: false // 클라이언트에서만 렌더링
 })
 
 export default function Page() {
-	return (
-		<div>
-			<h1>This is the alertAll/page.tsx</h1>
-			{/* AlertAll 컴포넌트 사용 */}
-			<AlertAll />
-		</div>
-	)
+  return (
+    <div>
+      <h1>This is the alertAll/page.tsx</h1>
+      {/* AlertAll 컴포넌트 사용 */}
+      <AlertAll />
+    </div>
+  )
 }
