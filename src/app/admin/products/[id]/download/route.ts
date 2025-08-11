49b9b559cde9a019fdd2fa8c696fb1fe1e52b3c4
ManI <letz0703@gmail.com>
2025-08-11 21:31:32 +0900

    ..

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -1,25 +1,30 @@
+// src/app/admin/products/[id]/download/route.ts
 import db from "@/db/db"
 import {notFound} from "next/navigation"
-import {NextRequest, NextResponse} from "next/server"
 import fs from "fs/promises"
 
-export async function GET(
-  req: NextRequest,
-  {params: {id}}: {params: {id: string}}
-) {
+// 💡 fs 사용하므로 node 런타임 강제
+export const runtime = "nodejs"
+
+export async function GET(_req: Request, {params}: {params: {id: string}}) {
+  const {id} = params
+
   const product = await db.product.findUnique({
     where: {id},
     select: {filePath: true, name: true}
   })
 
-  if (product == null) return notFound()
+  if (!product) return notFound()
 
   const {size} = await fs.stat(product.filePath)
   const file = await fs.readFile(product.filePath)
-  const extension = product.filePath.split(".").pop()
-  return new NextResponse(file, {
+  const extension = product.filePath.split(".").pop() ?? ""
+
+  return new Response(file, {
     headers: {
-      "Content-Disposition": `attachment; filename="${product.name}.${extension}`,
+      "Content-Type": "application/octet-stream",
+      // ✅ 닫는 따옴표 추가
+      "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
       "Content-Length": size.toString()
     }
   })
