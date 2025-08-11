aff86b423725f0da31ab74ed4440680399de5dee
ManI <letz0703@gmail.com>
2025-08-11 23:33:25 +0900

    package.json

---
+++ src/api/types/global.d.ts
@@ -0,0 +1,9 @@
+import type { PrismaClient } from "@prisma/client"
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+export {}
--- src/db/db.ts
+++ src/db/db.ts
@@ -4,12 +4,10 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-declare global {
-  let prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
 const db = globalThis.prisma ?? prismaClientSingleton()
 
-export default db
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db
+}
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+export default db
