ddf8309186fa77351c5a7d33506edc65347181be
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2026-02-05 16:58:23 +0900

    .

--- src/db/db.ts
+++
@@ -1,7 +0,0 @@
-//import {PrismaClient} from "@prisma/client"
-
-//const prismaClientSingleton = () => new PrismaClient()
-//const db = globalThis.prisma ?? prismaClientSingleton()
-//if (process.env.NODE_ENV !== "production") globalThis.prisma = db
-
-//export default db
