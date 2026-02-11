bd27882450aca6a27e25831c4187eebfa39ad6af
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2026-02-11 18:17:23 +0900

    ㄴ

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,34 @@
-import {NextRequest, NextResponse} from "next/server"
-//import {isValidPassword} from "./db/isValidPassword"
+//import {NextRequest, NextResponse} from "next/server"
+////import {isValidPassword} from "./db/isValidPassword"
 
-export async function middleware(req: NextRequest) {
-  if ((await isAuthenticated(req)) === false) {
-    return new NextResponse("Unauthorized", {
-      status: 401,
-      headers: {"WWW-Authenticate": "Basic"}
-    })
-  }
-}
+//export async function middleware(req: NextRequest) {
+//  //if ((await isAuthenticated(req)) === false) {
+//  //  return new NextResponse("Unauthorized", {
+//  //    status: 401,
+//  //    headers: {"WWW-Authenticate": "Basic"}
+//  //  })
+//  //}
+//}
 
-async function isAuthenticated(req: NextRequest) {
-  const authHeader =
-    req.headers.get("authorization") || req.headers.get("Authorization")
+//async function isAuthenticated(req: NextRequest) {
+//  const authHeader =
+//    req.headers.get("authorization") || req.headers.get("Authorization")
 
-  if (authHeader == null) return false
+//  if (authHeader == null) return false
 
-  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":")
+//  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+//    .toString()
+//    .split(":")
 
-  return (
-    username === process.env.ADMIN_USERNAME &&
-    (await isValidPassword(
-      password,
-      process.env.ADMIN_HASHED_PASSWORD as string
-    ))
-  )
-}
+//  return (
+//    username === process.env.ADMIN_USERNAME &&
+//    (await isValidPassword(
+//      password,
+//      process.env.ADMIN_HASHED_PASSWORD as string
+//    ))
+//  )
+//}
 
-export const config = {
-  matcher: "/admin/:path*"
-}
+//export const config = {
+//  matcher: "/admin/:path*"
+//}
